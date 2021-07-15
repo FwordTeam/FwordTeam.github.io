5212ed3bb53d575e399edca671a74ba2264ed0e1
root <root@EY.EY>
2021-07-15 07:52:55 -0400

    fwordctf 21 changes

--- js/countdown.js
+++ js/countdown.js
@@ -9,10 +9,10 @@ let countDown = new Date(Date.UTC(2021, 7, 29, 17, 0, 0, 0)).getTime(),
       let now = new Date().getTime(),
           distance = countDown - now;
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+      // document.getElementById('days').innerText = Math.floor(distance / (day)),
+      //   document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+      //   document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+      //   document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
       //do something later when date is reached
       //if (distance < 0) {
